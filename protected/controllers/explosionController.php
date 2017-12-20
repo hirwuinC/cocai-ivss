eb2c53235153f2192044956ab54f0951793ae9ef
Hirwuin <hirwuincol5@gmail.com>
2017-12-20 15:59:36 -0400

    Ya se puede hacer la explosion con subrecetas hasta cierto punto, aun falta hacer las conversiones y los calculos para posteriormente poder insertar en la tabla productos_has_explosion y finalmente poder consultar las explosiones, se cambio la validacion de si la tienda ya cargo el ranking del dia en curso, para ello se agrego un campo en la base de datos con la fecha del archivo y si esa fecha es igual al dia actual se dispara la validacion

--- protected/controllers/explosionController.php
+++ protected/controllers/explosionController.php
@@ -56,12 +56,12 @@
 		            closedir($dir);
 		        }
 		    }
-			//$this->_explosion->listar_archivos($idt);
+			
 			echo json_encode($arreglo);
 		}
 
 		public function validarcierre($hoy,$idt){
-			$query ="SELECT * from explosion where fecha = '".$hoy."' and unidad_negocio_id = $idt";
+			$query ="SELECT * from explosion where fecha_ranking = '".$hoy."' and unidad_negocio_id = $idt";
 			$data = $this->_main->select($query);
 			echo json_encode($data);
 		}
@@ -71,6 +71,10 @@
 			$ruta = $directorio.$fecha."-".$codigot.".txt";
          	$lineas = count(file($ruta));
          	$fechaexp = date('Y-m-d');
+         	$dia = substr($fecha, -2);
+         	$mes = substr($fecha, -4, 2);
+         	$year = substr($fecha, 0, 4);
+         	$fecharanking = $year."-".$mes."-".$dia;
 
          //echo $ruta."<br>".$valor['t']."<br>".$lineas."<br>"; 
          $fp= fopen($ruta,"r");
@@ -88,30 +92,46 @@
                             'costo'=>$costo,
                             'costoTotal'=>$costoTotal,
                             'porcentajeCosto'=>$porcentajeCosto);
-        	$rec[] = $this->ingredientesexplosion($info[$j]['codigo'],$idt,false);
-        	
-            for ($m=0; $m <=count($rec) ; $m++) { 
-            		if (isset($rec[$m][$j]['recetaing'])) {
-						$idrece = $rec[$m][$j]['recetaing'];
-						$sub[] = $this->subrecetas($idrece);
-						$rec[] = $sub;
-					}
-            }
+            $receid[] = $this->validarrecetas($info[$j]['codigo']);
             
-        }
-        for ($i=0; $i <count($info) ; $i++) { 
-    	$query = "INSERT INTO `explosion`(`fecha`, `grupo`, `codigo_prod`, `producto`, `cantidad`, `monto`, `porcentaje_vta`, `costo`, `costo_total`, `porcentaje_costo`, `unidad_negocio_id`) 
-    		VALUES ('".$fechaexp."','".$info[$i]['grupo']."','".$info[$i]['codigo']."','".$info[$i]['descripcion']."','".$info[$i]['cantidad']."','".$info[$i]['monto']."','".$info[$i]['porcentajeVentas']."','".$info[$i]['costo']."','".$info[$i]['costoTotal']."','".$info[$i]['porcentajeCosto']."',$idt)";
+            $query = "INSERT INTO `explosion`(`fecha`, `fecha_ranking`, `grupo`, `codigo_prod`, `producto`, `cantidad`, `monto`, `porcentaje_vta`, `costo`, `costo_total`, `porcentaje_costo`, `unidad_negocio_id`) 
+    		VALUES ('".$fechaexp."','".$fecharanking."','".$info[$j]['grupo']."','".$info[$j]['codigo']."','".$info[$j]['descripcion']."','".$info[$j]['cantidad']."','".$info[$j]['monto']."','".$info[$j]['porcentajeVentas']."','".$info[$j]['costo']."','".$info[$j]['costoTotal']."','".$info[$j]['porcentajeCosto']."',$idt)";
     		$explo[] = $this->_main->insertar($query);
-		}print_r($rec);
-    		
+            if (!isset($receid[$j][0]['receta_id'])) {
+            	$this->resultadoexplosion($info[$j],$explo[$j]);
+            }else{
+            	$rec = $this->ingredientesexplosion($info[$j]['codigo'],$idt,$info[$j]['cantidad']);
+        	
+	            for ($m=0; $m <count($rec) ; $m++) { 
+	            	//echo $rec[$m]['recetaing']."<br>";
+	            		if (isset($rec[$m]['recetaing'])) {
+							$idrece = $rec[$m]['recetaing'];
+							$sub = $this->subrecetas($idrece,$explo[$j]);
+							//echo "rec"; print_r($rec);  echo "<br>";
+							//echo "if cantidad: ".$rec[$m]['cantidad']."<br>";
+						}else{
+							//echo "en el otro else<br>";
+							//echo "rec"; print_r($rec);  echo "<br>";
+							//echo "else cantidad: ".$rec[$m]['cantidad']."<br>";
+							$this->resultadoexplosion($rec[$m],$explo[$m]/*,$rec[0]['cantidad'],$rec[0]['unidad_medida_id']*/);
+						}
+	            }
+            }
+        	
+            
+        }//print_r($receid); echo "sip <br><br>";
+        //print_r($rec);
+    		//echo count($receid); print_r($receid);
     	}
         
         //$separado = $this->separaringredientes($ingredientes);
                 //print_r($rec);
 		
 
-		function ingredientesexplosion($info,$idt){
+		function ingredientesexplosion($info,$idt,$cantidad){
+			//echo($info)."<br>";
+			//echo($idt)."<br>";
+			//echo($cantidad)."<br>";
 				for ($i=0; $i <count($info) ; $i++) { 
 					$query = "SELECT mercancia.id as idi, mercancia.codigo as codigi, mercancia.contenido_neto, mhudn.existencia, ixr.cantidad, ixr.unidad_medida_id, unidad_medida.abreviatura, CONCAT(mercancia.nombre, ' ', mercancia.marca) as ingrediente, producto.id as idprod, producto.nombre as producto, format(mercancia.precio_unitario,2,'de_DE') as costo, mercancia.precio_unitario as precioU, receta.id as idreceta, mercancia.receta_id as recetaing, receta.nombre as receta, unidad_sistema.id as 'idUMS',unidad_sistema.unidad as 'unidadS', unidad_sistema.abreviatura as 'abreviaturaS', unidad_presentacion.id as 'idUMP',unidad_presentacion.unidad as 'unidadP', unidad_presentacion.abreviatura as 'abreviaturaP',unidad_compra.id as 'idUMC',unidad_compra.unidad as 'unidadC', unidad_compra.abreviatura as 'abreviaturaC', formula_c, formula_p, formula_s, cantidad_presentacion
 	        	FROM `ingrediente_has_receta` as ixr
@@ -127,13 +147,16 @@
 				inner join unidad_medida as unidad_compra on unidad_compra.id = mercancia.unidad_medida_compra_id
 				WHERE producto.codigo = '".$info."' and unidad_negocio.id = $idt ";
 	    		$ingredientes[] = $this->_main->select($query);
+	    		//echo "ingredientes:"; print_r($ingredientes); echo "<br>";
 		    		for ($j=0; $j <=count($ingredientes) ; $j++) {
 						if (isset($ingredientes[$i][$j]['idi'])) {
+							//echo $ingredientes[$i][$j]['idi'];
 							$nuevo[] = array('iding'=>$ingredientes[$i][$j]['idi'],
 		                               	 'codigoing' =>$ingredientes[$i][$j]['codigi'],
 		                                 'cantidad'=>$ingredientes[$i][$j]['cantidad'],
 		                                 'abreviatura'=>$ingredientes[$i][$j]['abreviatura'],
 		                                 'ingredientes'=>$ingredientes[$i][$j]['ingrediente'],
+		                                 'unidad_medida_id'=>$ingredientes[$i][$j]['unidad_medida_id'],
 		                                 'costo'=>$ingredientes[$i][$j]['costo'],
 		                                 'idreceta'=>$ingredientes[$i][$j]['idreceta'],
 		                                 'recetaing'=>$ingredientes[$i][$j]['recetaing'],
@@ -142,18 +165,18 @@
 						}					 					
 					}
 				}
+				//print_r($nuevo);
+
 			return $nuevo;
 		}
 
-		function subrecetas($idreceta){
-			$query = "SELECT mercancia.id as idi, mercancia.receta_id, mercancia.codigo as codigi, mercancia.receta_id as recetaing, ixr.cantidad, abreviatura, mercancia.nombre as producto, CONCAT(mercancia.nombre, ' ', mercancia.marca) as ingrediente, format(mercancia.precio_unitario,2,'de_DE') as costo, mercancia.precio_unitario as precioU, ixr.receta_id as idreceta, receta.nombre as receta, CONCAT(mercpadre.nombre, ' ', mercpadre.marca) as ingredientep, mercpadre.id as idpadre
+		function subrecetas($idreceta,$explo){
+			//echo "id=";print_r($idreceta); echo "<br><br>";
+			$query = "SELECT mercancia.id as idi, mercancia.receta_id, mercancia.codigo as codigi, mercancia.receta_id as recetaing, ixr.cantidad, abreviatura, mercancia.nombre as producto, CONCAT(mercancia.nombre, ' ', mercancia.marca) as ingrediente, format(mercancia.precio_unitario,2,'de_DE') as costo, mercancia.precio_unitario as precioU, ixr.receta_id as idreceta, receta.nombre as receta, ixr.unidad_medida_id
 					FROM `mercancia`
 					inner join ingrediente_has_receta as ixr on mercancia.id = ixr.ingrediente_id
 					inner join receta on receta.id = ixr.receta_id
                     inner join unidad_medida on unidad_medida.id = ixr.unidad_medida_id
-                    left join receta as receta2 on receta2.id = mercancia.receta_id
-                    left join ingrediente_has_receta as ixr2 on ixr2.receta_id = receta2.id
-                    left join mercancia as mercpadre on ixr2.ingrediente_id = mercpadre.id
                     WHERE ixr.receta_id = $idreceta";
 					$ingredientes = $this->_main->select($query);
 					//print_r($ingredientes);
@@ -164,17 +187,41 @@
 		                                 'cantidad'=>$ingredientes[$k]['cantidad'],
 		                                 'abreviatura'=>$ingredientes[$k]['abreviatura'],
 		                                 'ingredientes'=>$ingredientes[$k]['ingrediente'],
+		                                 'unidad_medida_id'=>$ingredientes[$k]['unidad_medida_id'],
 		                                 'costo'=>$ingredientes[$k]['precioU'],
 		                                 'idreceta'=>$ingredientes[$k]['idreceta'],
-		                                 'recetaing'=>$ingredientes[$k]['recetaing'],
-		                             	 'idpadre'=>$ingredientes[$k]['idpadre'],
-		                                 'padre'=>$ingredientes[$k]['ingredientep']);
+		                                 'recetaing'=>$ingredientes[$k]['recetaing']);
 							
 						}
-						
+						if (isset($subreceta[$k]['recetaing'])) {
+							//echo $subrece[$k]['recetaing']."<br>";
+							$idrc = $subreceta[$k]['recetaing'];
+							$subs = $this->subrecetas($idrc,$explo);
+							//echo "en el if<br>";
+							for ($i=0; $i <count($subs) ; $i++) { 
+								if (!isset($subs[$i]['recetaing'])) {
+									$this->resultadoexplosion($subs[$i],$explo/*,$subs[0]['cantidad'],$subs[0]['unidad_medida_id']*/);
+								}
+								
+							}
+							
+						}
 					 					
-					}//print_r($subreceta);
+					}
+					
 					return $subreceta;
+		}
+
+		function resultadoexplosion($receta){
+			echo "a";print_r($receta); echo "<br><br>";
+			//exit();
+		}
+
+		function validarrecetas($codigop){
+			$query = "SELECT id, receta_id FROM producto where codigo ='".$codigop."'";
+			$datosprod = $this->_main->select($query);
+			//print_r($datosprod);
+			return $datosprod;
 		}
 
 			
